b1b739469599eb7122f2fd7abbfdd12325088924
Khoa <daikhivanthanh@gmail.com>
2023-11-13 13:34:05 +0700

    kt chinh phuong

--- dh52005906.cpp
+++ dh52005906.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 using namespace std;
-
+int IsCP(int n)
+{
+    if((int)sqrt(n)==sqrt(n)) return 1;
+    return 0;
+}
 int main()
 {
     int n;
     cout<<"nhap mot so nguyen: ";
     cin>>n;
+    if(IsCP(n)) cout<<"Day la so chinh phuong";else cout<<"Day khong phai so chinh phuong";
     return 0;
 }
