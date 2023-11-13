33f0c61ffba5836dcd6b1c14af7a53739826ef67
Khoa <daikhivanthanh@gmail.com>
2023-11-13 13:40:20 +0700

    kt hoan thien

--- dh52005906.cpp
+++ dh52005906.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 using namespace std;
-int IsCP(int n)
+int IsHT(int n)
 {
-    if((int)sqrt(n)==sqrt(n)) return 1;
-    return 0;
+    int s=0;
+    for(int i=1;i<n;i++)
+    if (n%i==0) s+=i;
+    return s==n;
 }
 int main()
 {
     int n;
     cout<<"nhap mot so nguyen: ";
     cin>>n;
-    if(IsCP(n)) cout<<"Day la so chinh phuong";else cout<<"Day khong phai so chinh phuong";
+    if(IsHT(n)) cout<<"Day la so hoan thien";else cout<<"Day khong phai so hoan thien";
     return 0;
 }
