0c7d6cfba027a88539cb4481a4bc46f44ae5db05
Khoa <daikhivanthanh@gmail.com>
2023-11-13 13:47:23 +0700

    update file

--- dh52005906.cpp
+++ dh52005906.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 using namespace std;
+int IsCP(int n)
+{
+    if((int)sqrt(n)==sqrt(n)) return 1;
+    return 0;
+}
 int IsHT(int n)
 {
     int s=0;
@@ -12,6 +17,7 @@ int main()
     int n;
     cout<<"nhap mot so nguyen: ";
     cin>>n;
+    if(IsCP(n)) cout<<"Day la so chinh phuong";else cout<<"Day khong phai so chinh phuong";
     if(IsHT(n)) cout<<"Day la so hoan thien";else cout<<"Day khong phai so hoan thien";
     return 0;
 }
